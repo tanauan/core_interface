6f072cb7c8441243617af2e71cb02b98a4636e76
tanauan <tanauan.cardoso@gmail.com>
2017-09-15 17:41:01 -0300

    ops

--- src/control.vhd
+++ src/control.vhd
@@ -240,7 +240,7 @@ begin
         -- EOP at word 2
         when x"08" | x"09" | x"0A" | x"0B" => shift_calc <= (others=>'0');
         -- EOP at word 3
-        when x"0C" | x"0D" | x"0E" | x"0F" => shift_calc <= (sop_location(2 downto 0) + 1) - 3;
+        when x"0C" | x"0D" | x"0E" | x"0F" => shift_calc <= (others=>'0');
         -- EOP at word 4
         when x"10" | x"11" | x"12" | x"13" => shift_calc <= (sop_location(2 downto 0) + 1) - 4;
         -- EOP at word 5
